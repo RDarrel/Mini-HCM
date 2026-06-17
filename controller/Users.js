bae7b40d4d46a53307e117cc6b82dd32839ba796
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-06-17 16:57:51 +0800

    update

--- controller/Users.js
+++ controller/Users.js
@@ -2,14 +2,13 @@ const { db } = require("../config/firebase");
 exports.browse = async (req, res) => {
   try {
     const snapshot = await db.collection("users").get();
-
-    const users = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-
-    res.json(users);
-    res.json({ message: "Attendance Browse Successfully" });
+    const users = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    res.json({ message: "Users Browse Successfully", payload: users });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
