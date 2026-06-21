bf32c36d59d6946ac2e1a6e521cda20ca74b05a1
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-06-22 02:00:25 +0800

    update

--- client/src/pages/platforms/administrator/access.js
+++ client/src/pages/platforms/administrator/access.js
@@ -1,11 +1,18 @@
-import { Gauge } from "lucide-react";
+import { Gauge, FileBarChart } from "lucide-react";
 import Dashboard from "./dashboard";
+import Reports from "./reports";
 const access = [
   {
     name: "Dashboard",
     path: "/dashboard",
     icon: Gauge,
     component: Dashboard,
+  },
+  {
+    name: "Reports",
+    path: "/reports",
+    icon: FileBarChart,
+    component: Reports,
   },
 ];
 
