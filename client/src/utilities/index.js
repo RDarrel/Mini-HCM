ec5953fb5f4f50073c010542b4ba8221ee49d665
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-06-23 17:29:21 +0800

    Fix mobile sidebar overlay issue and update frontend build

--- client/src/utilities/index.js
+++ client/src/utilities/index.js
@@ -3,9 +3,9 @@ import Male from "../assets/male.jpg";
 import Female from "../assets/female.jpg";
 import Formatter from "./formatter";
 import { toISODate, getTimezone } from "./date";
-const ENDPOINT = "http://localhost:5000";
+// const ENDPOINT = "http://localhost:5000";
 //use this to if the system is deployed
-// const ENDPOINT = window.location.origin;
+const ENDPOINT = window.location.origin;
 
 const PresetImage = (gender) => {
   if (gender) return Male;
